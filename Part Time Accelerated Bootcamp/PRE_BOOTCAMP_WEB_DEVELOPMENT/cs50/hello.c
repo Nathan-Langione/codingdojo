195999c6de6416bbe6a0cfcef33a4f4207ad2fec
Nathan Langione <nlangione@wayfair.com>
2021-05-21 15:05:36 -0400

    I shouldve done this sooner

--- Part Time Accelerated Bootcamp/PRE_BOOTCAMP_WEB_DEVELOPMENT/cs50/hello.c
+++ Part Time Accelerated Bootcamp/PRE_BOOTCAMP_WEB_DEVELOPMENT/cs50/hello.c
@@ -1,6 +1,8 @@
 // run clang -o <new compiled/executable file name> <code file name> 
 //run file with ./<filename>
 
+// Can also use make <beginning of file name>
+//run file with ./<filename>
 #include <stdio.h>
 
 int main(void)
